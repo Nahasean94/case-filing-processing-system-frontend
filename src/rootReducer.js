1a37a67b9246e69b1854e6010683a724339e9f3c
Nahashon Njenga <nahasean94@hotmail.com>
2018-08-14 22:38:32 +0300

    Removed unnecessary support for redux

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -3,22 +3,12 @@ import loginReducers from "./reducers/loginReducers"
 import advocateLoginReducers from "./reducers/advocateLoginReducers"
 import courtAdminLoginReducers from "./reducers/courtAdminLoginReducers"
 import factReducers from "./reducers/factReducers"
-import caseCategoriesReducers from "./reducers/caseCategoriesReducers"
-import caseDescriptionReducers from "./reducers/caseDescriptionReducers"
-import individualDefendantReducers from "./reducers/individualDefendantReducers"
-import individualPlaintiffReducers from "./reducers/individualPlaintiffReducers"
-import organizationPlaintiffReducers from "./reducers/organizationPlaintiffReducers"
-import organizationDefendantReducers from "./reducers/organizationDefendantReducers"
+
 
 export default combineReducers({
     loginReducers,
     advocateLoginReducers,
     courtAdminLoginReducers,
     factReducers,
-    caseCategoriesReducers,
-    caseDescriptionReducers,
-    individualDefendantReducers,
-    individualPlaintiffReducers,
-    organizationDefendantReducers,
-    organizationPlaintiffReducers,
+
 })
